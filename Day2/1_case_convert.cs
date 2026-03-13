0e9138218916872724eaa24e33546e77ad81ed3e
Reason196 <hazelnkanyane96@gmail.com>
2026-03-13 06:59:15 +0200

    Add case conversion program to convert input to lowercase

---
+++ Day2/1_case_convert.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program {
+    static void ConvertToLowercase() {
+        string input = Console.ReadLine();
+        Console.WriteLine(input.ToLower());
+    }
+
+    static void Main() {
+        ConvertToLowercase();
+    }
+}
