3beec7f52df619937fca64f85b573d5bc3804b2f
Reason196 <hazelnkanyane96@gmail.com>
2026-03-13 06:58:00 +0200

    Add program to print string length

---
+++ Day2/0_str_length.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program {
+    static void PrintStringLength() {
+        string input = Console.ReadLine();
+        Console.WriteLine(input.Length);
+    }
+
+    static void Main() {
+        PrintStringLength();
+    }
+}
